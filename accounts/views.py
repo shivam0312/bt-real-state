ee4fe775012f8fd458daf3b2d599efdfd9d359a4
StrawHatShivam <iamgod1001@hotmail.com>
2018-12-29 03:45:16 +0530

    Contact Enquiries 1.0

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages, auth
 from django.contrib.auth.models import User
+from contacts.models import Contact
 
 # Create your views here.
 def register(request):
@@ -74,4 +75,9 @@ def logout(request):
         return redirect('index')
 
 def dashboard(request):
-    return render(request,'accounts/dashboard.html')
+    contact_list = Contact.objects.order_by('contact_date').filter(user_id=request.user.id)
+
+    context = {
+        'contacts':contact_list
+    }
+    return render(request,'accounts/dashboard.html',context)
